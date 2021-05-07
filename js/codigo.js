e0fcc85184eeaf57e4c3cabd650668d8dfd4caa9
Guillem Talayero Carrasco <Guillem>
2021-05-07 18:11:47 +0200

    first commit

---
+++ js/codigo.js
@@ -0,0 +1,8 @@
+function language() {
+    var idioma = document.getElementById('language').value
+    if (idioma == 'ES') {
+        window.location.href = '../index.html'
+    } else {
+        window.location.href = './en/index.html'
+    }
+}
